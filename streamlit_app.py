929518ed018351614a54f4dcc62466d9a9bc88a0
Somu Srinivas Budidha <79302050+srsomub@users.noreply.github.com>
2024-05-14 00:22:15 +0530

    Update streamlit_app.py
    Connection changes requirement due to shift from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
 # Import python packages
-import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
-session = get_active_session()  # enable only one time
+import streamlit as st 
+from snowflake.snowpark.functions import col 
 
-
-
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 
 # Write directly to the app
