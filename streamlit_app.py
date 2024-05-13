1923f8ad62c3f8903228e886360b50f85f90d812
Somu Srinivas Budidha <79302050+srsomub@users.noreply.github.com>
2024-05-14 01:29:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st 
-from snowflake.snowpark.functions import col 
+from snowflake.snowpark.functions as f
 
 cnx=st.connection("snowflake")
 session=cnx.session()
