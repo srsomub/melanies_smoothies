3169dcbfeb6f823be86c54072155e4a3dfc91538
Somu Srinivas Budidha <79302050+srsomub@users.noreply.github.com>
2024-05-14 12:56:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ if name:
 
 
 # load data from your database
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 
